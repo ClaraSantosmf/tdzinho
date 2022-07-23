f936cdb3dfa4cc3d139ae79e79dc01ee57753819
Henrique Andrade <henrique.ccda@gmail.com>
2022-07-23 01:20:09 -0300

    Mostrando listas.

--- listinha/models.py
+++ listinha/models.py
@@ -1,12 +1,28 @@
 from django.db import models
 
-# Create your models here.
 
-class Listar(models.Model):
-    task_name=models.CharField(max_length=20)
-    squad=models.CharField(choices=(('infra','infraestrutura'), ('vendas', 'vendas'), ('tech', 'tech'),
-                                    ('juridico', 'juridico')), max_length=10)
-    descricao=models.TextField(max_length=200)
-    ativo = models.BooleanField(default=False)
+class Lista(models.Model):
+    SQUAD = {
+        ('i', 'infraestrutura'),
+        ('v', 'vendas'),
+        ('t', 'tech'),
+        ('j', 'juridico')
+    }
+
+    _SQUAD_DICT = dict(
+        i = 'infraestrutura',
+        v = 'vendas',
+        t = 'tech',
+        j = 'juridico',
+    )
+
+    task_name = models.CharField(max_length=20)
+    squad = models.CharField(choices=SQUAD, max_length=1)
+    descricao = models.TextField(max_length=200)
+    feito = models.BooleanField(default=False)
+
     def __str__(self):
         return self.task_name
+
+    def squad_name(self):
+        return self._SQUAD_DICT[self.squad]
--- listinha/views.py
+++ listinha/views.py
@@ -1,14 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Listar
-# Create your views here.
+from django.shortcuts import render, resolve_url
+from django.http import HttpResponseRedirect
+from .models import Lista
+
 
 def home(request):
-    return render(request, 'listinha/home.html')
+    if request.method == 'POST':
+        nome_task = request.POST["task_name"]
+        squad = request.POST["squad"]
+        descricao = request.POST["descricao"]
 
-def listar_task(request):
+        Lista.objects.create(task_name=nome_task, squad=squad, descricao=descricao)
+
+        return HttpResponseRedirect(resolve_url('home'))
+
+    context = {
+        'tasks_ativas': Lista.objects.filter(feito=True),
+        'tasks_inativas': Lista.objects.filter(feito=False),
+        'squads': Lista.SQUAD
+    }
+
+    return render(request, 'listinha/home.html', context=context)
+
+
+def atualizar_status(request):
     nome_task = request.POST["task_name"]
-    squad = request.POST["squad"]
-    descricao = request.POST["descricao"]
-    Listar.objects.create(task_name=nome_task, squad=squad, descricao=descricao)
-    return HttpResponse("Salvo")
+    feito = request.POST["feito"]
+
+    tasks = Lista.objects.get(task_name=nome_task)
+
+    tasks.feito = feito
+
+    tasks.save()
+
+    return HttpResponseRedirect(resolve_url('home'))
